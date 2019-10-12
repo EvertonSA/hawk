3c9c2057b3f7e8fe25735f0796b0542a9fd4cbb2
Ciro Gomes <cristian.kes@gmail.com>
2019-10-12 00:41:08 -0300

    0.0.2.6 - remove user service while test tweets

--- src/main/java/in/arakaki/hawk/service/UserServiceImpl.java
+++ src/main/java/in/arakaki/hawk/service/UserServiceImpl.java
@@ -29,29 +29,5 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private ModelMapper modelMapper;
 
-    /**
-     * Search an existing user
-     *
-     * @param email
-     * @return
-     */
-    public UserDto findUserByEmail(String email) {
-        Optional<User> user = Optional.ofNullable(userRepository.findByEmail(email));
-        if (user.isPresent()) {
-            return modelMapper.map(user.get(), UserDto.class);
-        }
-        throw exception(USER, ENTITY_NOT_FOUND, email);
-    }
 
-    /**
-     * Returns a new RuntimeException
-     *
-     * @param entityType
-     * @param exceptionType
-     * @param args
-     * @return
-     */
-    private RuntimeException exception(EntityType entityType, ExceptionType exceptionType, String... args) {
-        return HawkException.throwException(entityType, exceptionType, args);
-    }
 }
